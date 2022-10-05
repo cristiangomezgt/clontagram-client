280aab630e14218caca39d6291496832db1d63f4
cristiangomezgt <cristianfergmz@gmail.com>
2022-10-05 00:39:43 -0600

    ✨ New Feature: add upload image section markup

--- src/Pages/Upload.tsx
+++ src/Pages/Upload.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "../Components/layout/Main";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
@@ -10,26 +10,49 @@ type Props = {
 };
 
 const Upload = (props: Props) => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [uploadingImage, setUploadingImage] = useState(false);
+
   return (
     <Main center>
       <div className="Upload">
         <form>
           <div className="Upload__image-section">
-          </div>
-            <textarea
-              name="caption"
-              className="Upload__caption"
-              required
-              maxLength={180}
-              placeholder="Caption"
+            <UploadImageSection
+              imageUrl={imageUrl}
+              uploadingImage={uploadingImage}
             />
-            <button className="Upload__submit" type="submit">
-              Post
-            </button>
+          </div>
+          <textarea
+            name="caption"
+            className="Upload__caption"
+            required
+            maxLength={180}
+            placeholder="Caption"
+          />
+          <button className="Upload__submit" type="submit">
+            Post
+          </button>
         </form>
       </div>
     </Main>
   );
+};
+
+const UploadImageSection = (props: any) => {
+  if (props.uploadingImage) {
+    return <Loading />;
+  } else if (props.imageUrl) {
+    return <img src={props.imageUrl} alt="" />;
+  } else {
+    return (
+      <label className="Upload__image-label">
+        <FontAwesomeIcon icon={faUpload}></FontAwesomeIcon>
+        <span>Publish a photo</span>
+        <input type="file" className="hidden" name="image" />
+      </label>
+    );
+  }
 };
 
 export default Upload;
