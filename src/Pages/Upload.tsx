8b15de06bc7983b0d8a5edd06aa7909306420037
cristiangomezgt <cristianfergmz@gmail.com>
2022-10-05 00:15:54 -0600

    ✨ New Feature: add upload component markup

--- src/Pages/Upload.tsx
+++ src/Pages/Upload.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import Main from '../Components/layout/Main'
+import React from "react";
+import Main from "../Components/layout/Main";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import Loading from "../Components/Loading";
+import Axios from "axios";
 
 type Props = {
-  showError: (message: string) => void
-}
+  showError: (message: string) => void;
+};
 
 const Upload = (props: Props) => {
   return (
-    <Main center><div>Upload</div></Main>
-  )
-}
+    <Main center>
+      <div className="Upload">
+        <form>
+          <div className="Upload__image-section">
+          </div>
+            <textarea
+              name="caption"
+              className="Upload__caption"
+              required
+              maxLength={180}
+              placeholder="Caption"
+            />
+            <button className="Upload__submit" type="submit">
+              Post
+            </button>
+        </form>
+      </div>
+    </Main>
+  );
+};
 
-export default Upload
+export default Upload;
