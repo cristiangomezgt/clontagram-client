c8c22d2c06484f866babec6dbc476339047c6175
cristiangomezgt <cristianfergmz@gmail.com>
2022-10-29 23:09:03 -0600

    🐛 Bug: fix navigate to feed after login/register

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 import Main from "./Components/layout/Main";
 import Loading from "./Components/Loading";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import { IUser } from "./Types/user.type";
 import Error from "./Components/Error";
@@ -27,6 +27,7 @@ export default function App() {
   const [user, setUser] = useState<IUser | null>(null);
   const [loadingUser, setLoadingUser] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const login = async (email: string, password: string) => {
     const { data } = await Axios.post("/api/usuarios/login", {
@@ -35,6 +36,7 @@ export default function App() {
     });
     setUser(data.usuario);
     setToken(data.token);
+    navigate('/')
   };
 
   useEffect(() => {
@@ -58,6 +60,7 @@ export default function App() {
     const { data } = await Axios.post("/api/usuarios/signup", user);
     setUser(data.usuario);
     setToken(data.token);
+    navigate('/')
   };
 
   const logout = () => {
@@ -81,27 +84,27 @@ export default function App() {
     );
   }
   return (
-    <BrowserRouter>
-      <Nav user={user!}/>
-      <Error message={error} hideError={hideError}></Error>
-      <Routes>
-        <Route path="/" element={<ProtectedRoute user={user!} showError={showError} />}>
-          <Route
-            index
-            element={<Feed user={user!} showError={showError} />}
-          />
-          <Route
-            path="/upload"
-            element={<Upload showError={showError} />}
-          />
-          <Route path="/post/:postId" element={<Post user={user!} showError={showError} />} />
-          <Route path="/explore" element={<Explore showError={showError} />} />
-          <Route path="/profile/:username" element={<Profile logout={logout} user={user!} showError={showError} />} />
-        </Route>
-        <Route path="/login" element={<Login login={login} showError={showError}/>} />
-        <Route path="/signup" element={<SignUp signup={signup} showError={showError}/>} />
-        <Route path="*" element={<Navigate to="/signup" />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <React.Fragment>
+        <Nav user={user!}/>
+        <Error message={error} hideError={hideError}></Error>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute user={user!} showError={showError} />}>
+            <Route
+              index
+              element={<Feed user={user!} showError={showError} />}
+            />
+            <Route
+              path="/upload"
+              element={<Upload showError={showError} />}
+            />
+            <Route path="/post/:postId" element={<Post user={user!} showError={showError} />} />
+            <Route path="/explore" element={<Explore showError={showError} />} />
+            <Route path="/profile/:username" element={<Profile logout={logout} user={user!} showError={showError} />} />
+          </Route>
+          <Route path="/login" element={<Login login={login} showError={showError}/>} />
+          <Route path="/signup" element={<SignUp signup={signup} showError={showError}/>} />
+          <Route path="*" element={<Navigate to="/signup" />}></Route>
+        </Routes>
+      </React.Fragment>
   );
 }
